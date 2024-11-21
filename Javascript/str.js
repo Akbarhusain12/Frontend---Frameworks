5ed05414145680529dd3ed6ba7f2a9118f0db8da
Kadiwala Akbar husain <akbarhusainkadiwala32@gmail.com>
2024-11-21 14:25:27 +0530

    Updated str file

--- Javascript/str.js
+++ Javascript/str.js
@@ -4,18 +4,45 @@
 // console.log(str[4]);
 
 
-// template literals
+// Slice
 
 // let text = `javascript`;
 // console.log(text);
 // slc = text.slice(2,6)
 // console.log(slc);
 
-let text = `javascript`;
-let text2 = 'Html'
-let text3 = 'css'
-slc = text.concat(text2, text3)
-console.log(slc);
+
+// concat
+// let text = `javascript`;
+// let text2 = 'Html'
+// let text3 = 'css'
+// slc = text.concat(text2, text3)
+// console.log(slc);
+
+// replace
+// let text = `javascript`;
+// rpl = text.replace("j","M")
+// console.log(rpl);
+
+
+
+// Practice
+
+// let userName = prompt("Enter your Full Name");
+
+// console.log("@" + userName + userName.length);
+
+
+// checking in string
+// let userName = prompt("Enter your Full Name");
+
+// let startName = userName.startsWith("@");
+
+// let endName = userName.endsWith("!"); 
+
+// let result = startName + " " + endName; 
+
+// console.log(result); 
 
 
 
