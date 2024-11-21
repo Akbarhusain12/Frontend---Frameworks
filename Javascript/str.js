ea40fe110430c98218d97c3815033ca7a9229fc6
Kadiwala Akbar husain <akbarhusainkadiwala32@gmail.com>
2024-11-21 14:10:29 +0530

    Update str.js

--- Javascript/str.js
+++ Javascript/str.js
@@ -1,12 +1,22 @@
-let str = "Akbarhusain"
-let value = str.length
-console.log(value);
-console.log(str[4]);
+// let str = "Akbarhusain"
+// let value = str.length
+// console.log(value);
+// console.log(str[4]);
 
 
 // template literals
 
-let text = `Hello "World"!`;
-console.log(text);
+// let text = `javascript`;
+// console.log(text);
+// slc = text.slice(2,6)
+// console.log(slc);
+
+let text = `javascript`;
+let text2 = 'Html'
+let text3 = 'css'
+slc = text.concat(text2, text3)
+console.log(slc);
+
+
 
 
