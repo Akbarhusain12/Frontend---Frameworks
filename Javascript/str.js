81cd0dba851ec25be3e6ae1d2d80addf9ca3a2e8
Kadiwala Akbar husain <akbarhusainkadiwala32@gmail.com>
2024-11-21 11:59:45 +0530

    Uploaded file for frontend

---
+++ Javascript/str.js
@@ -0,0 +1,12 @@
+let str = "Akbarhusain"
+let value = str.length
+console.log(value);
+console.log(str[4]);
+
+
+// template literals
+
+let text = `Hello "World"!`;
+console.log(text);
+
+
---
+++ Javascript/text.js
@@ -0,0 +1,94 @@
+// const accId = 1234
+// console.log(accId)
+
+// var bankId = 3456
+// console.log(bankId)
+
+// let a = 20
+// a = "Akbar"
+// console.log(a)
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i%2 == 0) {
+//         console.log(i)
+//     }
+// }
+
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// count = 100
+// i = 0
+// while(i <= count){
+//     if (i % 2 == 0) {
+//         console.log(i)
+//     }
+//     i++
+// }
+
+// count = 100;
+// i = 0;
+
+// do {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+//   i++
+// } while (i <= count);
+
+
+// let sttrr = 'ajsbfjdbjc';
+// size = 0
+// for (element of sttrr) {
+//   console.log(element);
+//   size++
+// }
+
+// console.log(size)
+
+// const object = { a: 1, b: 2, c: 3 };
+
+// for (property in object) {
+//   console.log(`${property}: ${object[property]}`);
+// }
+
+
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// create a game where you start with a random number -> ask the user to keep guessing the game number unitll they entered the correct one
+
+let x = 56;
+
+let user = prompt("Guess a number between 1 and 100:");
+
+// while(x != user){
+//     user = prompt("Guess a number between 1 and 100:");
+// }
+
+while (user != x) {
+    if (user < x) {
+        user = prompt("Too low! Try again:");
+    } else if (user > x) {
+        user = prompt("Too high! Try again:");
+    }
+  }
+
+// console.log(user)
+
+
+for (let i = 0; i < 5; i++) {
+  if (user === x) {
+    console.log("Number matched! Congratulations!");
+    break;
+  } else if (i < 4) {
+    user = parseInt(prompt("Wrong guess! Try again:"));
+  } else {
+    console.log("Sorry,The number was:", x);
+  }
+}
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
