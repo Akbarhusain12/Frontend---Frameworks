043a22a4ef9719e833d95675150ea970c6caa108
Kadiwala Akbar husain <akbarhusainkadiwala32@gmail.com>
2024-11-29 11:23:34 +0530

    Updated array.js file

--- Javascript/array.js
+++ Javascript/array.js
@@ -1,6 +1,30 @@
-let heros = ['IronMan', 'batman','Hulk']
+// let heros = ['IronMan', 'batman','Hulk']
 
-for (i of heros) {
-    console.log(i.toUpperCase());
+// for (i of heros) {
+//     console.log(i.toUpperCase());
+    
+// }
+
+
+// Q 1
+
+// let std = [85,84,95,90,50]
+
+// let avg = 0 
+
+// for (element of std) {
+//     avg = element + avg
+// }
+
+// console.log(avg/std.length);
+
+
+// Q 2
+let items = [250,645,300,900,50]
+
+let newarr = []
+
+for (i of items) {
     
 }
+console.log(newarr);
