d2ebf1efaa078c86a16a6408e5cfc45bc45056a3
Kadiwala Akbar husain <akbarhusainkadiwala32@gmail.com>
2024-12-02 16:52:27 +0530

    updated code

--- Javascript/function.js
+++ Javascript/function.js
@@ -7,16 +7,41 @@ function add(x,y) {
   console.log(value);
 */
 
-function str(x) {
-    let count = 0;
-    for (let i of x.toLowerCase()) {
-        if (i == 'a' || i == 'e' || i == 'i' || i == 'o' || i == 'u') {
-            count++
-        }
-    }
-    return count;
-}
+// function str(x) {
+//     let count = 0;
+//     for (let i of x.toLowerCase()) {
+//         if (i == 'a' || i == 'e' || i == 'i' || i == 'o' || i == 'u') {
+//             count++
+//         }
+//     }
+//     return count;
+// }
 
-console.log(str("Akbar husain"));
+// console.log(str("Akbar husain"));
 
+// let myFunction = (a, b) => a * b;
+// console.log(myFunction(10, 20));
+
+
+// const str = (x) => {
+//     let count = 0;
+//     for (let i of x.toLowerCase()) {
+//         if (i == 'a' || i == 'e' || i == 'i' || i == 'o' || i == 'u') {
+//             count++
+//         }
+//     }
+//     console.log(count);
+// }
+
+// str("Akbar husain");
+
+const numbers = [1, 2, 3, 4, 5];
+const doubled = [];
+
+numbers.forEach(num => {
+    doubled.push(num * 2);
+});
+
+console.log(doubled); 
+console.log(numbers);
 
